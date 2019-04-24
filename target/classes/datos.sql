1e2673af6b94abdd9f356c59301417fe9410559c
jjguerrero <jjguerrero@serikat.es>
2019-04-24 13:08:03 +0200

    Guardando cambios

---
+++ target/classes/datos.sql
@@ -0,0 +1,3 @@
+insert into persona (nombre, ape_paterno, email,fechalta) values ('Admin', 'admin', 'admin@icursos.net','2017-05-12'); 
+insert into persona (nombre, ape_paterno, email,fechalta) values ('Juan', 'Perez', 'juan@gmail.com','2018-05-12'); 
+insert into persona (nombre, ape_paterno, email,fechalta) values ('Juan', 'Rodriguez', 'jrodriguez@gmail.com','2019-05-12');
